583a389600e7ccdcaf956bedcb42b6443282bdb6
Rochelle Awuor <awuorochelle75@gmail.com>
2025-04-27 19:47:49 +0300

    modifies files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import "./index.css";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import routes from "./routes";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<RouterProvider router={routes} />);
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,27 @@
-
+import Home from "./pages/Home";
+import Actors from "./pages/Actors";
+import Directors from "./pages/Directors";
+import Movie from "./pages/Movie";
+import ErrorPage from "./pages/ErrorPage"; // optional for 404s
 
 const routes = [
-   
-  ];
+  {
+    path: "/",
+    Component: Home,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/actors",
+    Component: Actors
+  },
+  {
+    path: "/directors",
+    Component: Directors
+  },
+  {
+    path: "/movie/:id",
+    Component: Movie
+  }
+];
 
 export default routes;
